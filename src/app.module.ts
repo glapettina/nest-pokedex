560d55c9830cd4ec283a2bd6ee151c3a9ca63fb3
glapettina <guillermo@glsoftware.com.ar>
2024-06-14 18:20:37 -0300

    preparar mongodb y envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,25 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
 
-      MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+      MongooseModule.forRoot( process.env.MONGODB, {
+        dbName: 'pokemonsdb'
+      }),
 
     PokemonModule,
 
@@ -23,3 +34,5 @@ import { SeedModule } from './seed/seed.module';
   ],  
 })
 export class AppModule {}
+
+
